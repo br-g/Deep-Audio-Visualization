c954a1fdf5cc9cd92819754b000b35ff1ced3c5d
BGodefroyFR <godefroy@kth.se>
2017-05-06 14:59:36 +0200

    Added audio resampling

--- learning/data/extractSpectrograms.py
+++ learning/data/extractSpectrograms.py
@@ -4,28 +4,30 @@
 
 from pylab import *
 from scipy.io import wavfile
-from scipy.signal import spectrogram
+from scipy.signal import spectrogram, resample
 import matplotlib
 import sys
 import pickle
 
 import params
 
-params.WINDOW_SIZE = 512
-
 rawAudioPath = sys.argv[1]
 outFilePath = sys.argv[2]
 
-sampFreq, sound = wavfile.read(rawAudioPath)
+originalSampleFreq, audioData = wavfile.read(rawAudioPath)
 
-sound = sound / (2.**15) # Map values into [-1, 1]
+# If sample rate different from the standart one, resamples.
+if originalSampleFreq != params.SAMPLE_RATE:
+	print "Resampling..."
+	audioData = resample(audioData,
+		int(float(len(audioData)) * float(params.SAMPLE_RATE) / float(originalSampleFreq)))
 
-n = len(sound)
-spectrograms = np.empty((len(sound) / params.WINDOW_SIZE, params.WINDOW_SIZE))
+audioData = audioData / (2.**15) # Map values into [-1, 1]
+spectrograms = np.empty((len(audioData) / params.WINDOW_SIZE, params.WINDOW_SIZE))
 
 print "Extracting spectrogram..."
-for i in range(0, len(sound) / params.WINDOW_SIZE):
-	spectrograms[i,:] = abs( np.fft.fft(sound[i*params.WINDOW_SIZE:(i+1)*params.WINDOW_SIZE]) ) * 1000.
+for i in range(0, len(audioData) / params.WINDOW_SIZE):
+	spectrograms[i,:] = abs( np.fft.fft(audioData[i*params.WINDOW_SIZE:(i+1)*params.WINDOW_SIZE]) ) * 1000.
 
 print "Pickling data..."
 pickle.dump(spectrograms, open(outFilePath, "w"))
