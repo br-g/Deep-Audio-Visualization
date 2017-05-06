b5adfaeb50f59914199c610d1a0970bc7df2a71d
BGodefroyFR <bruno.godefroy@insa-lyon.fr>
2017-05-06 12:36:36 +0200

    Added LMDB data export

--- learning/data/extractSpectrograms.py
+++ learning/data/extractSpectrograms.py
@@ -1,3 +1,7 @@
+####
+# Extracts spectrograms from raw audio file and exports pickled data
+###
+
 from pylab import *
 from scipy.io import wavfile
 from scipy.signal import spectrogram
@@ -5,7 +9,9 @@ import matplotlib
 import sys
 import pickle
 
-windowSize = 512
+import params
+
+params.WINDOW_SIZE = 512
 
 rawAudioPath = sys.argv[1]
 outFilePath = sys.argv[2]
@@ -15,16 +21,16 @@ sampFreq, sound = wavfile.read(rawAudioPath)
 sound = sound / (2.**15) # Map values into [-1, 1]
 
 n = len(sound)
-spectrograms = np.empty((len(sound) / windowSize, windowSize))
+spectrograms = np.empty((len(sound) / params.WINDOW_SIZE, params.WINDOW_SIZE))
 
 print "Extracting spectrogram..."
-for i in range(0, len(sound) / windowSize):
-	spectrograms[i,:] = abs( np.fft.fft(sound[i*windowSize:(i+1)*windowSize]) ) * 1000.
+for i in range(0, len(sound) / params.WINDOW_SIZE):
+	spectrograms[i,:] = abs( np.fft.fft(sound[i*params.WINDOW_SIZE:(i+1)*params.WINDOW_SIZE]) ) * 1000.
 
 print "Pickling data..."
 pickle.dump(spectrograms, open(outFilePath, "w"))
 
 """for i in range (0, 10):
-	freqArray = arange(0, windowSize, 1.0);
+	freqArray = arange(0, params.WINDOW_SIZE, 1.0);
 	plot(freqArray, spectrograms[i,:], color='k')
 	plt.show()"""
