fe58919037092d816fa86911aad54335bfd3d6db
BGodefroyFR <godefroy@kth.se>
2017-05-12 15:27:23 +0200

    Started front end scripts

--- web-app/main.js
+++ web-app/main.js
@@ -1,20 +1,45 @@
-var nj = NumJS;
-loadModel();
+function Music (title, artist, FPS, features) {
+    this.title = title;
+    this.artist = artist;
+    this.FPS = FPS;
+    this.features = features;
 
-function loadModel() {
-	model = new Net.CaffeModel(
-		'models/autoencoder.prototxt',
-		'weights/weights1/'
-		);
+    this.toLog = function () {
+    	console.log(this.title);
+    	console.log(this.artist);
+    	console.log(this.FPS);
+    	console.log(this.features);
+    }
 
-	model.load().then(function(){
-		
-		var inputs = new Net.Vol(256, 1, 1, 0.0);
-		for(var i = 0; i < 256; i++) {
-			inputs.set(i, 0, 0, Math.random());
-		}
-
-		var outputs = model.forward(inputs);
-		console.log(outputs);
-	});
+    // Get features at given time in the music.
+    // Time in ms.
+    this.getFeatures = function (elapsedTime) {
+    	var frameIndex = Math.round(elapsedTime * this.FPS / 1000.0);
+    	console.log(frameIndex);
+    	return this.features[frameIndex];
+    }
 }
+
+var currentMusic = null;
+
+function loadMusic (filePath, callback) {
+	var music = null
+    $.getJSON(filePath)
+		.done(function(data) {
+		    console.log("Features successfully loaded");
+		    currentMusic = new Music(data['Title'], data['Artist'], data['FPS'], data['Features']);
+		    callback();
+		})
+		.fail(function() {
+			console.log( "Ajax error" );
+		});
+}
+
+function playMusic () {
+	var audio = new Audio('audio/m0_mono.wav');
+	audio.play();
+}
+
+$( document ).ready(function() {
+	loadMusic("features/test1.json", playMusic);
+});
