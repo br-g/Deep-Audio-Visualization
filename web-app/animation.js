269ed58fc89e52e16702d26f29b8c8847267d1b3
BGodefroyFR <godefroy@kth.se>
2017-05-16 23:16:20 +0200

    Added basic web-app and particle system

--- web-app/animation.js
+++ web-app/animation.js
@@ -1,39 +1,67 @@
-function Animation(features) {
-	var scene = null;
-	var camera = null;
-	var ctx = null;
+function AnimationManager() {
+	var ctx = null; // camera + scene
 	var anim = null;
 	var renderer = null;
+	var features = null;
+	var paramMapping = null;
 	var startTime = 0;
+	var lastRenderTime = 0;
 
 	this.init = function() {
-		scene = new THREE.Scene();
-		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		var scene = new THREE.Scene();
+		var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 		ctx = {'scene': scene, 'camera': camera}
 		renderer = new THREE.WebGLRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		document.body.appendChild( renderer.domElement );
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(renderer.domElement);
 	}
 
-	this.setMode = function(mode) {
-		switch (mode) {
+	this.setAnimation = function(animName) {
+		switch (animName) {
 			case 'cube':
 				anim = new Cube();
 				break;
+			case 'particles':
+				anim = new Particles();
+				break;
 			default:
-				console.log('Error: Unknow animation mode.');
+				console.log('Error: Unknow animation name.');
 		}
 		if (anim) anim.init(ctx);
+		loadParamMapping();
+	}
+
+	this.loadFeatures = function(filePath) {
+		features = new Features();
+		features.load(filePath);
+	}
+
+	this.onWindowResize = function() {
+		ctx['camera'].aspect = window.innerWidth / window.innerHeight;
+		ctx['camera'].updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+
+	function loadParamMapping() {
+		paramMapping = new ParamMapping();
+		paramMapping.load(anim.getPath() + '/parameters.json');
 	}
 
 	this.launch = function() {
 		startTime = new Date().getTime();
-		render();
+		lastRenderTime = startTime;
+		renderAnimation();
 	}
 
-	function render() {
-		requestAnimationFrame(render);
+	this.randomize = function() {
+		paramMapping.randomize();
+	}
+
+	function renderAnimation () {
+		requestAnimationFrame(renderAnimation);
 		renderer.render(ctx['scene'], ctx['camera']);
-		anim.update(features.get(new Date().getTime() - startTime));
+		var curTime = new Date().getTime();
+		anim.update((curTime - lastRenderTime) / 1000.0, paramMapping.doMap(features.get(curTime - startTime)));
+		lastRenderTime = curTime;
 	}
 };
