d8f3884876de020d5b41116dc317518dad49d4d2
BGodefroyFR <godefroy@kth.se>
2017-05-28 23:44:07 +0200

    Added controls in web-app

--- web-app/audio.js
+++ web-app/audio.js
@@ -4,8 +4,11 @@ function AudioManager () {
     this.load = function (filePath) {
         this.audio = new Audio(filePath);
     }
-
     this.playMusic = function () {
     	this.audio.play();
+    }
+    this.pauseMusic = function () {
+    	if (this.audio != null)
+    		this.audio.pause();
     }
 }
