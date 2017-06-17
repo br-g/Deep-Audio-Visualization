c0835697c4e3eea0c0d30aad950ab8cc9637c4af
BGodefroyFR <bruno.godefroy@insa-lyon.fr>
2017-06-17 22:00:29 +0200

    Updated scripts

--- learning/scripts/addTrainingSamplesToLMDB.py
+++ learning/scripts/addTrainingSamplesToLMDB.py
@@ -6,45 +6,17 @@
 ###
 
 import sys
-import os
-import downloadTrainingSamples
 import extractSpectrograms
 import toLMDB
 
-def emptyFolder(path):
-	filelist = [ f for f in os.listdir(path) ]
-	for f in filelist:
-	    os.remove(path + "/" + f)
-
-def addTrainingSamples(LmdbfilePath, nbSamples):
-	batchSampleCount = 0
-
-	emptyFolder('../raw_audio')
-	downloadTrainingSamples.downloadFromYouTube('../raw_audio/m', batchSize)
-
-	filelist = [ f for f in os.listdir('../raw_audio') ]
-	for f in filelist:
-		os.system('./rawAudioToMono.sh ../raw_audio/' + f)
-		os.remove('../raw_audio/' + f)
-
-	emptyFolder('../spectro_data')
-	filelist = [ f for f in os.listdir('../raw_audio') ]
-	for f in filelist:
-		pickleFilePath = '../spectro_data/' + f[:-4] + '.pk'
-		spectrograms = extractSpectrograms.extractSpectrogram('../raw_audio/' + f, True)
-		if spectrograms != None:
-			toLMDB.toLMDB(spectrograms, LmdbfilePath)
-			batchSampleCount += 1
-
-	return batchSampleCount
+def addTrainingSamples(rawAudiofilePath, lmdbPath):
+	spectrograms = extractSpectrograms.extractSpectrogram(rawAudiofilePath, True)
+	if spectrograms != None:
+		toLMDB.toLMDB(spectrograms, lmdbPath)
+	else:
+		print("Error")
 
 if __name__ == "__main__":
-	LmdbfilePath = sys.argv[1];
-	nbSamples = int(sys.argv[2]);
-	batchSize = int(sys.argv[3]);
-
-	sampleCount = 0
-
-	while sampleCount < nbSamples:
-		sampleCount += addTrainingSamples(LmdbfilePath, batchSize)
-		print("--- sample count: " + str(sampleCount))
+	rawAudiofilePath = sys.argv[1];
+	lmdbPath = sys.argv[2];
+	addTrainingSamples(rawAudiofilePath, lmdbPath)
