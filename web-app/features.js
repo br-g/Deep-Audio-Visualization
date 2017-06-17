f27a677f28074933e9e235ab95aa3490c762df69
BGodefroyFR <godefroy@kth.se>
2017-06-17 22:06:46 +0200

    Added blur in animation management

--- web-app/features.js
+++ web-app/features.js
@@ -1,27 +1,31 @@
 function Features () {
-    this.FPS = null;
+    this.sampleRate = null;
     this.features = null;
 
     this.load = function (filePath) {
-        var _title, _artist, _FPS, _features;
+        var _title, _artist, _sampleRate, _features;
         $.ajax({
           url: filePath,
           dataType: 'json',
           async: false,
           success: function(data) {
             console.log("Features successfully loaded");
-            _FPS = data['FPS'];
-            _features = data['Features'];
+            _sampleRate = data['sampleRate'];
+            _features = data['featuresData'];
           }
         });
-        this.FPS = _FPS;
+        this.sampleRate = _sampleRate;
         this.features = _features;
     }
 
     // Get features at given time in the music.
     // Time in ms.
     this.get = function (elapsedTime) {
-    	var frameIndex = Math.round(elapsedTime * this.FPS / 1000.0);
-    	return this.features[frameIndex];
+    	var frameIndex = Math.round(elapsedTime * this.sampleRate / 1000.0);
+      var frame = {}
+      for (var b in this.features) {
+        frame[b] = this.features[b][frameIndex];
+      }
+    	return /*this.features["blur" + blur][frameIndex]*/ frame;
     }
 }
